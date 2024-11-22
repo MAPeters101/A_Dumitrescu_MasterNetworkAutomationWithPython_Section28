6aac9a8a94bf5daeb2af91ebacd93215769097cb
Mark Peters <mapeters101@earthlink.net>
2024-11-22 12:34:40 -0500

    Lesson246:  Work on lesson to 6:45.

---
+++ lesson246.py
@@ -0,0 +1,29 @@
+from turtle import Turtle, Screen
+
+my_screen = Screen()
+donatello = Turtle()
+print(my_screen.canvwidth)
+donatello.shape('turtle')
+donatello.color('purple')
+donatello.forward(100)
+donatello.right(90)
+donatello.forward(100)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+my_screen.exitonclick()
+
