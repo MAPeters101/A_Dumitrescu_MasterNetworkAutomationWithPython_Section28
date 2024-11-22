cc822b5a95f286c3666d3ccf77bd9fb4e2a75b01
Mark Peters <mapeters101@earthlink.net>
2024-11-22 12:41:17 -0500

    Lesson246:  Complete work on lesson from 6:45.

--- lesson246.py
+++ lesson246.py
@@ -8,15 +8,25 @@ donatello.color('purple')
 donatello.forward(100)
 donatello.right(90)
 donatello.forward(100)
+donatello.right(90)
+donatello.forward(100)
+donatello.right(90)
+donatello.forward(200)
+donatello.home()
 
+raphael = Turtle()
+raphael.color('red')
+raphael.shape('turtle')
+raphael.penup()
+raphael.goto(-150, 200)
+raphael.pendown()
+raphael.pencolor('blue')
 
-
-
-
-
-
-
-
+x = 10
+while x <= 50:
+    raphael.circle(x)
+    donatello.circle(x+5)
+    x += 10
 
 
 
