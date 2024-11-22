221e6396d795ecd9e914d308571a358c1419b83f
Mark Peters <mapeters101@earthlink.net>
2024-11-22 14:44:05 -0500

    Lesson250:  Work on lesson to 6:50.

---
+++ lesson250.py
@@ -0,0 +1,40 @@
+# a, b = 4, 5
+# x, z = '4', '5'
+#
+# print(a + b)
+# print(x + z)
+# print(a.__add__(b))
+# print(x.__add__(z))
+# print(x.__add__('jjjjj'))
+#
+#
+
+class Robot:
+    """This class implements a Robot."""
+    population = 0
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+        Robot.population += 1
+
+    def __del__(self):
+        print(f'Robot {self.name} destroyed!')
+
+    def __str__(self):
+        my_str = f'My name is {self.name} and my price is {self.price}.'
+        return my_str
+
+
+r1 = Robot('Marvin', 150)
+r2 = Robot('Gal', 45)
+
+#print(r1 + r2)
+#print(r1 > r2)
+print(r1)
+
+
+
+
+
+
+
